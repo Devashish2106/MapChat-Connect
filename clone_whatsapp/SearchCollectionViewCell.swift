097958156388d30648e5d9cae8a09d9db1a6c1ec
Devashish2106 <devghanshani4@gmail.com>
2024-06-11 16:44:47 +0530

    Added pagination functnality to the SearchViewController and made the cell clicked update the image

---
+++ clone_whatsapp/SearchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SearchCollectionViewCell.swift
+//  clone_whatsapp
+//
+//  Created by Devashish Ghanshani on 11/06/24.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var displayImage: UIImageView!
+    
+}
--- clone_whatsapp/SearchViewController.swift
+++ clone_whatsapp/SearchViewController.swift
@@ -26,9 +26,12 @@ protocol SearchViewControllerDelegate: AnyObject {
     func editSC (_ vc: SearchViewController, search: UIImage)
 }
 
-class SearchViewController: UIViewController, UISearchBarDelegate {
+class SearchViewController: UIViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var searchBar: UISearchBar!
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var currentQuery : String = ""
     
     var results : [Result] = []
     
@@ -37,29 +40,39 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         
 
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
         searchBar.delegate = self
     }
     
+    var arr: [UIImage] = []
+    var totalPg = 0
+    var curPg = 1
+    
     weak var delegate: SearchViewControllerDelegate?
     
-    @IBAction func exitBtn(_ sender: Any) {
-        if let image : UIImage = searchImage.image {
-            delegate?.editSC(self, search: image)
-        }
-    }
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if let text = searchBar.text {
             searchBar.resignFirstResponder()
             results = []
-            searchImage.reloadInputViews()
-            fetchPhotos(query: text)
+            arr = []
+            currentQuery = text
+            curPg = 1
+            collectionView.reloadData()
+            fetchPhotos(query: text, page: curPg)
         }
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let image: UIImage = arr[indexPath.row]
+        delegate?.editSC(self, search: image)
+    }
     
-    func fetchPhotos(query : String) {
+    func fetchPhotos(query : String, page: Int) {
         
-        let urlString = "https://api.unsplash.com/search/photos?page=1&per_page=1&query=\(query)&client_id=s94OAY6V3ulSyVUxuj_a6_BJUUWbpeL_u3v_BYhl4EE"
+        guard page <= totalPg || page == 1 else { return }
+        
+        let urlString = "https://api.unsplash.com/search/photos?page=\(page)&per_page=30&query=\(query)&client_id=s94OAY6V3ulSyVUxuj_a6_BJUUWbpeL_u3v_BYhl4EE"
         
         guard let url = URL(string: urlString) else {
             return
@@ -72,9 +85,10 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
                 
             do {
                 let jsonResult = try JSONDecoder().decode(Response.self, from: data)
-                self?.results = jsonResult.results
-                if let firstResult = jsonResult.results.first {
-                    self?.configure(with: firstResult.urls.regular)
+                self?.results.append(contentsOf: jsonResult.results)
+                self?.totalPg = jsonResult.total_pages
+                for result in jsonResult.results {
+                    self?.configure(with: result.urls.regular)
                 }
             } catch {
                 print(error)
@@ -94,11 +108,32 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
                 }
                 let image = UIImage(data: data)
                 DispatchQueue.main.async {
-                    self?.searchImage.image = image
+                    self?.arr.append(image!)
+                    self?.collectionView.reloadData()
                 }
             }
             task.resume()
         }
 
-    @IBOutlet weak var searchImage: UIImageView!
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return totalPg
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SearchCollectionViewCell
+        cell.displayImage.image = arr[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == arr.count - 1 && curPg < totalPg {
+            curPg += 1
+            fetchPhotos(query: currentQuery, page: curPg)
+        }
+    }
 }
